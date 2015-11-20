a96251ca99d6f876f9247ff8ea1bbca3f34ddf8f
Peter Kempf <pk@ekse.de>
2015-11-20 12:49:49 +0100

    new includes

--- portofolio.pl
+++ portofolio.pl
@@ -14,13 +14,12 @@ eval 'exec perl -wS $0 ${1+"$@"}'
 
 # Letzte Aenderung: 
 
-use warnings;    # on by default
-
-#use utf8;      # so literals and identifiers can be in UTF-8
-#use v5.12;     # or later to get "unicode_strings" feature
-#use strict;    # quote strings, declare variables
-#use warnings  qw(FATAL utf8);    # fatalize encoding glitches
-#use open      qw(:std :utf8);    # undeclared streams in UTF-8
+use utf8;      # so literals and identifiers can be in UTF-8
+use v5.12;     # or later to get "unicode_strings" feature
+use strict;    # quote strings, declare variables
+use warnings  qw(FATAL utf8);    # fatalize encoding glitches
+ 
+use open      qw(:std :utf8);    # undeclared streams in UTF-8
 #use charnames qw(:full :short);  # unneeded in v5.16
 #binmode STDOUT, ":utf8";
 
